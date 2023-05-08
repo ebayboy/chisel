7a556fe47b5fe43dd57ec34ee2e8e9e5055b36c2
fanpengfei1 <fanpengfei@jd.com>
2023-05-08 03:43:48 +0000

    update for more log debug info.

--- share/tunnel/tunnel_out_ssh.go
+++ share/tunnel/tunnel_out_ssh.go
@@ -13,10 +13,12 @@ import (
 	"golang.org/x/crypto/ssh"
 )
 
+// * 健康检查处理函数：
 func (t *Tunnel) handleSSHRequests(reqs <-chan *ssh.Request) {
 	for r := range reqs {
 		switch r.Type {
 		case "ping":
+			t.Debugf("Recv ping, then Reply pong ...") //Recv ping, then Reply pong
 			r.Reply(true, []byte("pong"))
 		default:
 			t.Debugf("Unknown request: %s", r.Type)
@@ -24,19 +26,25 @@ func (t *Tunnel) handleSSHRequests(reqs <-chan *ssh.Request) {
 	}
 }
 
+// * 业务请求处理函数-1
 func (t *Tunnel) handleSSHChannels(chans <-chan ssh.NewChannel) {
 	for ch := range chans {
+		t.Debugf("handleSSHChannels ch:%v", &ch)
 		go t.handleSSHChannel(ch)
 	}
 }
 
+// * 业务请求处理函数-2
 func (t *Tunnel) handleSSHChannel(ch ssh.NewChannel) {
+
+	t.Debugf("handleSSHChannel ch:%v", &ch)
 	if !t.Config.Outbound {
 		t.Debugf("Denied outbound connection")
 		ch.Reject(ssh.Prohibited, "Denied outbound connection")
 		return
 	}
 	remote := string(ch.ExtraData())
+
 	//extract protocol
 	hostPort, proto := settings.L4Proto(remote)
 	udp := proto == "udp"
@@ -51,19 +59,24 @@ func (t *Tunnel) handleSSHChannel(ch ssh.NewChannel) {
 		t.Debugf("Failed to accept stream: %s", err)
 		return
 	}
+
 	stream := io.ReadWriteCloser(sshChan)
+	t.Debugf("accept sshChan:[%v]  stream:[%v]", sshChan, stream)
+
 	//cnet.MeterRWC(t.Logger.Fork("sshchan"), sshChan)
 	defer stream.Close()
 	go ssh.DiscardRequests(reqs)
 	l := t.Logger.Fork("conn#%d", t.connStats.New())
+
 	//ready to handle
 	t.connStats.Open()
-	l.Debugf("Open %s", t.connStats.String())
+	l.Debugf("Open %s socks:[%v] hostPort:[%v]", t.connStats.String(), socks, hostPort) //Open [1/1] socks:[false] hostPort:[test05.testtest05.com:8080]
 	if socks {
 		err = t.handleSocks(stream)
 	} else if udp {
 		err = t.handleUDP(l, stream, hostPort)
 	} else {
+		//* Goto here
 		err = t.handleTCP(l, stream, hostPort)
 	}
 	t.connStats.Close()
@@ -79,11 +92,13 @@ func (t *Tunnel) handleSocks(src io.ReadWriteCloser) error {
 }
 
 func (t *Tunnel) handleTCP(l *cio.Logger, src io.ReadWriteCloser, hostPort string) error {
+
+	l.Debugf("handleTCP: hostPort:[%v]", hostPort)
 	dst, err := net.Dial("tcp", hostPort)
 	if err != nil {
 		return err
 	}
 	s, r := cio.Pipe(src, dst)
-	l.Debugf("sent %s received %s", sizestr.ToString(s), sizestr.ToString(r))
+	l.Debugf("handleTCP sent %s received %s", sizestr.ToString(s), sizestr.ToString(r))
 	return nil
 }
